7d7a3451f2497f77aeac0f1b90b3e299cd026f43
Shane Richman <Shane.M.Richman@gmail.com>
2017-10-06 03:33:37 +0000

    initial commit for socialNetwork.js with names of functions in place

---
+++ socialNetwork.js
@@ -0,0 +1,70 @@
+var data = {
+  f01: {
+    name: "Alice",
+    age: 15,
+    follows: ["f02", "f03", "f04"]
+  },
+  f02: {
+    name: "Bob",
+    age: 20,
+    follows: ["f05", "f06"]
+  },
+  f03: {
+    name: "Charlie",
+    age: 35,
+    follows: ["f01", "f04", "f06"]
+  },
+  f04: {
+    name: "Debbie",
+    age: 40,
+    follows: ["f01", "f02", "f03", "f05", "f06"]
+  },
+  f05: {
+    name: "Elizabeth",
+    age: 45,
+    follows: ["f04"]
+  },
+  f06: {
+    name: "Finn",
+    age: 25,
+    follows: ["f05"]
+  }
+};
+
+// List everyone and for each of them, list the names of who they follow and who follows them
+var whoFollowsWho = function() {
+
+};
+
+// Identify who follows the most people
+var followsMostPeople = function() {
+
+};
+
+// Identify who has the most followers
+var mostFollowers = function() {
+
+};
+
+// Identify who has the most followers over 30
+var mostFollowersAbove = function() {
+
+};
+
+// Identify who follows the most people over 30
+var followsMostPeopleAbove = function () {
+
+};
+
+// List those who follow someone that doesn't follow them back
+var Stalkers = function() {
+
+};
+
+// List everyone and their reach (sum of # of followers and # of followers of followers)
+var network = function() {
+
+};
+
+
+
