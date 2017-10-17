495233c81135ed65d7780857b3e8a0b428a22e2c
Shane Richman <Shane.M.Richman@gmail.com>
2017-10-17 16:10:00 +0000

    got all but one function working in socialNetwork, still needs refactoring

--- socialNetwork.js
+++ socialNetwork.js
@@ -31,18 +31,25 @@ var data = {
   }
 };
 
-// Helper function to list followers of given person
 var whoFollows = function(ID) {
-  var followersArray = [];
-  for (var profile in data) {
+  let followersArray = [];
+  for (let profile in data) {
     if (data[profile].follows.includes(ID)) {
       followersArray.push(data[profile].name);
     }
   }
-  // console.log(followersArray);
   return followersArray;
 };
-// whoFollows("f05");
+
+var whoFollowsAbove = function(ID, age) {
+  let followersArray = [];
+  for (let profile in data) {
+    if (data[profile].age > age && data[profile].follows.includes(ID)) {
+      followersArray.push(data[profile].name);
+    }
+  }
+  return followersArray;
+}
 
 var idToName = function(arr) {
   const listOfNames = [];
@@ -52,12 +59,22 @@ var idToName = function(arr) {
   // console.log(listOfNames);
   return listOfNames;
 };
-// idToName(["f02", "f03", "f04"]);
+
+var followsAbove = function(ID, age) {
+  let followsArray = [];
+  for (let person of data[ID].follows) {
+    if (data[person].age > age) {
+      followsArray.push(data[person].name);
+    }
+    // console.log(person);
+  }
+  return followsArray;
+}
 
 // List everyone and for each of them, list the names of who they follow and who follows them
 var whoFollowsWho = function() {
   var listWhoFollowsWho = {};
-  for (var person in data) {
+  for (let person in data) {
     listWhoFollowsWho[data[person].name] = {
       follows: idToName(data[person].follows),
       hasFollowing: whoFollows(person)
@@ -66,42 +83,93 @@ var whoFollowsWho = function() {
   console.log(listWhoFollowsWho);
   return listWhoFollowsWho;
 };
-/*
-var final = {
-  bob: {
-    follows: [Elizabeth, Finn],
-    hasFollowing: [Debbie, Elizabeth, Finn]
-  },
-  Charlie: [Alice, Debbie]
-};
-*/
-whoFollowsWho();
-
+// whoFollowsWho();
 
 // Identify who follows the most people
 var followsMostPeople = function() {
-
+  let mostFollows = 0;
+  let personMostFollows = "";
+  for (let person in data) {
+    if (data[person].follows.length > mostFollows) {
+      mostFollows = data[person].follows.length;
+      personMostFollows = data[person].name;
+    }
+  }
+  return personMostFollows;
 };
+// console.log(followsMostPeople());
 
 // Identify who has the most followers
 var mostFollowers = function() {
-
+  let mostFollowers = 0;
+  let personMostFollowers = "";
+  for (let person in data) {
+    const currentFollowers = whoFollows(person).length;
+    if (currentFollowers > mostFollowers) {
+      mostFollowers = currentFollowers;
+      personMostFollowers = data[person].name;
+    }
+  }
+  return personMostFollowers;
 };
+// console.log(mostFollowers());
 
 // Identify who has the most followers over 30
-var mostFollowersAbove = function() {
-
+var mostFollowersAbove = function(age) {
+  let mostFollowers = 0;
+  let personMostFollowers = "";
+  for (let person in data) {
+    const currentFollowers = whoFollowsAbove(person, age).length;
+    if (currentFollowers > mostFollowers) {
+      mostFollowers = currentFollowers;
+      personMostFollowers = data[person].name;
+    }
+  }
+  return personMostFollowers;
 };
+// console.log(mostFollowersAbove(30));
 
 // Identify who follows the most people over 30
-var followsMostPeopleAbove = function() {
-
+var followsMostPeopleAbove = function(age) {
+  let mostFollows = 0;
+  let personMostFollows = "";
+  for (let person in data) {
+    const peopleAboveAge = followsAbove(person, age).length;
+    if (peopleAboveAge > mostFollows) {
+      mostFollows = peopleAboveAge;
+      personMostFollows = data[person].name;
+    }
+  }
+  return personMostFollows;
 };
+// console.log(followsMostPeopleAbove(30));
 
 // List those who follow someone that doesn't follow them back
 var Stalkers = function() {
+  let stalkerArray = [];
+  for (let profile in data) {
+    const follows = idToName(data[profile].follows);
+    const followers = whoFollows(profile);
+    let keepGoing = true;
 
+    follows.forEach(function(name) {
+      if (!followers.includes(name) && keepGoing) {
+        stalkerArray.push(data[profile].name);
+        keepGoing = false;
+      }
+    });
+
+    for (var name of follows) {
+      if (!followers.includes(name) && keepGoing) {
+        stalkerArray.push(data[profile].name);
+        keepGoing = false;
+      }
+    }
+
+  }
+  return stalkerArray;
 };
+// console.log(Stalkers());
 
 // List everyone and their reach (sum of # of followers and # of followers of followers)
 var network = function() {
